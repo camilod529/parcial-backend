90e9e1d315f6f0abf6d81c0a45741ea3f3b90630
FateDGG <anghelandres12@gmail.com>
2023-09-19 18:53:20 -0500

    Commit 3

--- seccion_2.php
+++ seccion_2.php
@@ -30,21 +30,31 @@
         function validarFormulario() {
             var edad = parseInt(document.getElementById("edad_asistente").value);
 
-            if (edad < masjoven && edad != 0){
+            if (edad < masjoven && edad != 0 && edad >= 18){
                 masjoven = edad;
             }
 
             if (edad < 18 && edad > 0) {
-                document.getElementById("resultado").innerHTML = "Error: Los menores de edad no tienen permitida la entrada";
+                alert ("Error: Los menores de edad no tienen permitida la entrada");
+                document.getElementById("edad_asistente").value = "";
+                document.getElementById("resultado").innerHTML = "";
                 return false; 
             } else if (edad == 0){
 
                 document.getElementById("resultado").innerHTML = "La cantidad de personas que asistieron fue: " + cantidad + "<br>La edad promedio de asistentes fue: " + promedio + "<br>La edad del menor asistente fue: " + masjoven;
+                document.getElementById("edad_asistente").value = "";
+                masjoven = Infinity; 
+                promediar = 0;
+                cantidad = 0;
+                promedio = 0;
                 return false; 
+
             } else {
                 cantidad = cantidad + 1;
                 promediar = promediar + edad;
                 promedio = promediar/cantidad;
+                document.getElementById("edad_asistente").value = "";
+                document.getElementById("resultado").innerHTML = "";
                 return false;
             }
         }
