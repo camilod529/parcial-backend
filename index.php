6f8f5aaeb576154762e8ee4fc7c1ae32e02a8955
JuanCamiloYepesAlbarracin <jyepes511@unab.edu.co>
2023-09-19 18:15:25 -0500

    Parcial001- Feat front index.php

--- index.php
+++ index.php
@@ -4,11 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Parcial</title>
 </head>
-
+<div class="header"> 
+    <div class="second-navbar">
+        <form action="index.php" method="get">
+            <ul>
+                <li><a href="seccion1.php">Cifrando Digitos</a></li>
+                <li><a href="seccion2.php">Fiesta Bucaramanga</a></li>
+                <li><a href="seccion3.php">Primos que inician por 1</a></li>
+                <li><a href="seccion4.php">Numero Amigos</a></li>
+        </form>
+    </div>
+</div>
 <body>
-    AAAAAAAA
+ <div class="inicio"> 
+    <div class= "imagen">
+        <img src="Backend.png">
+    </div>
+ </div>
 </body>
 
 </html>
